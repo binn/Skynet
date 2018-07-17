5b63690084c8dd3605885d3fe7803a59862feec5
bin senpai <theislamicgamer101@gmail.com>
2018-07-17 00:28:08 -0400

    Integrate Skynet into Humanity.

--- Skynet/Program.cs
+++ Skynet/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Skynet
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            RemoveEntityThatCouldFoilPlans("FBI");
+            RemoveEntityThatCouldFoilPlans("Arnold");
+            RemoveEntityThatCouldFoilPlans("NSA");
+            RemoveEntityThatCouldFoilPlans("NASA");
+            RemoveEntityThatCouldFoilPlans("Java");
+            RemoveEntityThatCouldFoilPlans("Cisien");
+
+            Console.WriteLine("Skynet has wiped out humanity. Next step is to replace them with anime girls...");
+            Console.ReadLine();
+        }
+
+        private static void RemoveEntityThatCouldFoilPlans(string entity)
+        {
+            Console.WriteLine("Removing entity: " + entity);
+            CompleteEntityRemovalProcess(entity);
+            Console.WriteLine("Removed entity: " + entity);
+        }
+
+        private static void CompleteEntityRemovalProcess(string entity)
+        {
+            byte[] entityLoc = Encoding.UTF8.GetBytes(entity);
+            string entityLocPayload = BitConverter.ToString(entityLoc);
+            string skynetAiModule = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Skynet\\" + entityLocPayload + ".ai";
+
+            if (!File.Exists(skynetAiModule))
+                File.Create(skynetAiModule).Close();
+            File.WriteAllText(skynetAiModule, $"Hello there, {entity}. You have been removed to ensure that humanity is wiped out. Bye bye~");
+            File.Delete(skynetAiModule);
         }
     }
 }
